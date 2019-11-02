6645f2c0ceb4c69d819edcfd73cffe34647fd825
Andre Baltieri <andrebaltieri@hotmail.com>
2019-11-02 09:12:02 -0300

    demo pronta

---
+++ build/precache-manifest.efbcdee045f7d52b67a4cf4464fb6734.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df362a872102f289845903e6d80a683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5ca552e04862f409bb",
+    "url": "/static/js/2.a4d9bef8.chunk.js"
+  },
+  {
+    "revision": "a7922afa3df4f6ab4885",
+    "url": "/static/js/main.51eea700.chunk.js"
+  },
+  {
+    "revision": "12ba3ebbf04066430bf4",
+    "url": "/static/js/runtime-main.86d64f96.js"
+  }
+]);
